9fc034e11ce1c4b70ad2ec692917eb44b998b59b
Juan Mejia Isaza <juanes.reich@gmail.com>
2024-11-03 21:41:47 -0500

    cretea component carros

---
+++ carros/src/app/lista-carros/lista-carros.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-carros',
+  standalone: true,
+  imports: [],
+  templateUrl: './lista-carros.component.html',
+  styleUrl: './lista-carros.component.css'
+})
+export class ListaCarrosComponent {
+
+}
